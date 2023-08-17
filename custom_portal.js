5f7aa030e71e74ccbbbeff6858d32232d7b8b2d6
Andrés Villarreal <andres@villarreal.co.cr>
2023-08-17 15:15:24 +0200

    Fix random bg

--- custom_portal.js
+++ custom_portal.js
@@ -1,7 +1,9 @@
 var init_portal_original = init_portal;
 var init_portal = function() {
     init_portal_original();
+    var script = document.querySelector("script[src$='custom_portal.js']");
+    var base = script.src.replace("custom_portal.js", "");
     var bgImages = ["bg1.jpg", "bg2.jpg", "bg3.jpg"];
     var i = Math.floor(Math.random() * bgImages.length);
-    document.body.style.backgroundImage = `url(${bgImages[i]})`;
+    document.body.style.backgroundImage = `url(${base + bgImages[i]})`;
 };
